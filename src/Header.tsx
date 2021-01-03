44770f8c8d6397b2cb316b6bb6d94ecc8a246a0c
utouto97 <utouto97@gmail.com>
2021-01-04 02:03:22 +0900

    update Header (add image and some sample text)

--- src/Header.tsx
+++ src/Header.tsx
@@ -5,13 +5,29 @@ import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import IconButton from '@material-ui/core/IconButton';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+import Link from '@material-ui/core/Link';
+
 import MenuIcon from '@material-ui/icons/Menu';
+import TwitterIcon from '@material-ui/icons/Twitter';
+import GitHubIcon from '@material-ui/icons/GitHub';
+import BookIcon from '@material-ui/icons/Book';
+
+import headerImg from './header.jpg';
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
     title: {
       flexGrow: 1,
       textAlign: 'center',
+    },
+    headerPaper: {
+      backgroundImage: `url(${headerImg})`,
+      backgroundSize: 'cover'
+    },
+    noDecoration: {
+      textDecoration: 'none'
     }
   })
 );
@@ -26,12 +42,44 @@ const Header = () => {
           <IconButton edge='start' color='inherit' aria-label='menu'>
             <MenuIcon />
           </IconButton>
-          <Typography variant='h6' className={classes.title}>
-            Aoki Tarou
-          </Typography>
         </Toolbar>
       </AppBar>
-    </React.Fragment>
+      <Paper className={classes.headerPaper}>
+        <Grid container alignItems='center' justify='center'>
+          <Grid item md={6}>
+            <Typography variant='h3'>
+              Firstname Lastname
+            </Typography>
+            <Typography variant='h5'>
+              bio bio bio bio bio <br />
+              bio bio bio bio bio <br />
+              bio bio bio bio bio <br />
+              bio bio bio bio bio <br />
+              bio bio bio bio bio <br />
+            </Typography>
+            <br />
+            <Link display='block' variant='body2' href='https://twitter.com'>
+              <Grid container direction='row' spacing={1} alignItems='center'>
+                <Grid item><TwitterIcon fontSize='small' /></Grid>
+                <Grid item>@xyzxyz12</Grid>
+              </Grid>
+            </Link>
+            <Link display='block' variant='body2' href='https://github.com'>
+              <Grid container direction='row' spacing={1} alignItems='center'>
+                <Grid item><GitHubIcon fontSize='small' /></Grid>
+                <Grid item>xyzxyz12</Grid>
+              </Grid>
+            </Link>
+            <Link display='block' variant='body2' href='https://qiita.com'>
+              <Grid container direction='row' spacing={1} alignItems='center'>
+                <Grid item><BookIcon fontSize='small' /></Grid>
+                <Grid item>あいうえお</Grid>
+              </Grid>
+            </Link>
+          </Grid>
+        </Grid>
+      </Paper>
+    </React.Fragment >
   );
 };
 
--- src/Home.tsx
+++ src/Home.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Home = () => {
   return (
     <React.Fragment>
-      <p>Body</p>
+      Body
     </React.Fragment>
   );
 };
